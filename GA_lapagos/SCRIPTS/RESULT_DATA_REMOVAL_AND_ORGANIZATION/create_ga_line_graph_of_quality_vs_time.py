0a8c2ddd07b32d06c9ebd9019da74035904f770c
drpaj12 <jamieson.peter@gmail.com>
2020-04-06 20:54:28 -0400

    Updates for the python plotting
    - Scripts backups and experimental files

---
+++ GA_lapagos/SCRIPTS/RESULT_DATA_REMOVAL_AND_ORGANIZATION/create_ga_line_graph_of_quality_vs_time.py
@@ -0,0 +1,69 @@
+import sys
+import os
+import subprocess
+from pathlib import Path
+import glob
+import pandas as pd
+import numpy as np
+import re
+
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
+#matplotlib.use("pgf")
+#matplotlib.rcParams.update({
+#    "pgf.texsystem": "pdflatex",
+#    'font.family': 'serif',
+#    'text.usetex': True,
+#    'pgf.rcfonts': False,
+#})
+
+# 0_script 
+# 1_witdh of the columns {7}
+# 2_regex_of_files
+# 3_number_of_colors
+# SAMPLE: create_spread_sheet.py "files*files" 7 out.csv
+print ("Number of arguments: %d" %  len(sys.argv))
+print ("Argument List: %s" % str(sys.argv))
+
+NUM_COLORS = int(sys.argv[3])
+line_style=['-','--','-.',':']
+# start the plot
+f, ax = plt.subplots()
+# set up a color cylcle based on NUM_COLORS
+cm = plt.get_cmap('gist_rainbow')
+ax.set_prop_cycle(color=[cm(1.*i/NUM_COLORS) for i in range(NUM_COLORS)])
+
+# argument 1 is max number of columns which we use
+column_header_list =  [str(x) for x in range(int(sys.argv[1]))]
+
+list_legend = []
+
+# get all the files to analyze
+file_list = glob.glob('*'+sys.argv[2].rstrip()+'*')
+
+print (file_list)
+
+count = 0
+# read through the file and create a data frame for each benchmark
+for log_file in file_list:
+    count = count + 1
+    frame = pd.read_csv(log_file, names=column_header_list)
+    # extract details
+    cross_name = re.search('XS_(.+?)_XE', log_file).group(1)
+    problem_type_name = re.search('PS_ADJACENCY_(.+?)_PE', log_file).group(1)
+    # add to the list that will be the legend
+    list_legend.append(cross_name + ' ' + problem_type_name)
+
+    # do a step plot and set to x = 0 as start
+    ax.step(frame.loc[frame['1'].str.contains('generation')]['2'],
+            frame.loc[frame['1'].str.contains('generation')]['6'],
+            linestyle=line_style[count % 4])
+    ax.set_xlim(0)
+
+plt.legend(list_legend)
+
+plt.xlabel('Generations')
+plt.ylabel('Quality')
+plt.savefig('bmark_quality_vs_generation_bm.png', dpi=400)
